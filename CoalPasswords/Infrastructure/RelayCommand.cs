c43ff521f14606016b9dab6e0d8ffabe852eac77
Leonid <list.leonid@gmail.com>
2019-01-17 23:56:23 +0200

    Initial commit. Log In page basic design done.

---
+++ CoalPasswords/Infrastructure/RelayCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace CoalPasswords
+{
+    class RelayCommand : ICommand
+    {
+        readonly Action<object> action;
+        readonly Predicate<object> predicate;
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+        public RelayCommand(Action<object> action, Predicate<object> predicate=null)
+        {
+            this.action = action;
+            this.predicate = predicate;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return predicate == null ? true : predicate.Invoke(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            action?.Invoke(parameter);
+        }
+    }
+}
---
+++ CoalPasswords/ViewModel/Data.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace CoalPasswords
+{
+    class Data : INotifyPropertyChanged
+    {
+        public ICommand CloseWindow { get; set; }
+        private string login;
+        private string password;
+        public string Login
+        {
+            get => login;
+            set
+            {
+                login = value;
+                Notify();
+            }
+        }
+        public string Password
+        {
+            get => password;
+            set
+            {
+                password = value;
+                Notify();
+            }
+        }
+
+        public int CallerMemberName { get; private set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public Data()
+        {
+            CloseWindow = new RelayCommand(x => System.Windows.Application.Current.Shutdown());
+        }
+        private void Notify([CallerMemberName]string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
