1cdc68ca8ff0656b34917c8897d557ce5b41e627
Leonid <list.leonid@gmail.com>
2019-01-18 16:52:40 +0200

    Sign in and Sign up button works now

--- CoalPasswords/ViewModel/Data.cs
+++ CoalPasswords/ViewModel/Data.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace CoalPasswords
@@ -11,22 +12,67 @@ namespace CoalPasswords
         public event PropertyChangedEventHandler PropertyChanged;
         public ICommand CloseWindow { get; set; }
         public ICommand AddUser { get; set; }
+        public ICommand SignIn { get; set; }
         private IRepository<User> UsersRepository { get; set; }
         public string EnteredLogin { get; set; }
-        public string EnteredPassword { get; set; }
+        private string EnteredPassword { get; set; }
+        private string _signUpStatusString;
+        public string SignUpStatusString
+        {
+            get => _signUpStatusString;
+            set
+            {
+                _signUpStatusString = value;
+                Notify();
+            }
+        }
         public Data()
         {
             UsersRepository = new Repository<User>();
             CloseWindow = new RelayCommand(x => System.Windows.Application.Current.Shutdown());
-            AddUser = new RelayCommand(x => { UsersRepository.Add(new User(EnteredLogin, EnteredPassword)); });
+            AddUser = new RelayCommand(ExecuteAddingUser);
+            SignIn = new RelayCommand(ExecuteSigningIn);
         }
-        public void ShowUsers()
+        private void ExecuteSigningIn(object param)
         {
-            foreach(var user in UsersRepository.GetAll())
+            if (UsersRepository.GetAll() is List<User> list)
             {
-                Console.WriteLine(user.Login + " " + user.Password);
-                Console.WriteLine("Records: " + (user.PasswordRecords.GetAll() as IList<IRecord>).Count);
+                var user = list.Find(x => x.Login == EnteredLogin);
+                if (user != null)
+                {
+                    var pass = param as PasswordBox;
+                    EnteredPassword = pass.Password;
+                    if(user.Password == EnteredPassword)
+                        SignUpStatusString = "Succesfully signed in.";
+                    return;
+                }
+                SignUpStatusString = "This username not exist.";
+                return;
             }
+            SignUpStatusString = "Error in Users repository.";
+            throw new Exception("Users repositpry not List<User>");
+        }
+        private void ExecuteAddingUser(object param)
+        {
+            if(UsersRepository.GetAll() is List<User> list)
+            {
+                if(list.FindAll(x => x.Login == EnteredLogin).Count == 0)
+                {
+                    var pass = param as PasswordBox;
+                    EnteredPassword = pass.Password;
+                    UsersRepository.Add(new User(EnteredLogin, EnteredPassword));
+                    SignUpStatusString = "Succesfully signed up.";
+                    return;
+                }
+                SignUpStatusString = "This username already exist.";
+                return;
+            }
+            SignUpStatusString = "Error in Users repository.";
+            throw new Exception("Users repositpry not List<User>");
+        }
+        private void Notify([CallerMemberName]string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
     }
 }
